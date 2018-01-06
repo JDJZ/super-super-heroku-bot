4d816114df52347ac81fa50fb09d197334a325e3
JDJZ <JDJZ@users.noreply.github.com>
2018-01-05 16:29:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
-const YTDL = require('ytdl-core');
 
 var prefix = config.prefix;
 
